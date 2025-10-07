556834071c84602735106416a52dad09a3d3e877
ArinLamba <arinlamba5@gmail.com>
2025-10-08 00:07:09 +0530

    initial 2

--- src/server.ts
+++ src/server.ts
@@ -7,28 +7,24 @@ import registerEvents from "./events";
 const app = express();
 const server = http.createServer(app);
 
-// app.use(cors({ origin: "http://localhost:3000" })); // frontend URL
-// app.use(express.json());
+app.use(cors({ origin: process.env.CORS_ORIGIN || "http://localhost:3000" })); // frontend URL
+app.use(express.json());
 
-// Example REST API route
-app.get("/api", (req, res) => {
-  res.json({ msg: "Backend is running" });
-});
 
 // Start Socket.IO
 const io = new Server(server, {
   cors: { 
-    origin: process.env.HOST || "http://localhost:3000", 
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000", 
     methods: ["GET", "POST"],
     credentials: true
   }
 });
 
 io.on("connection", (socket) => {
-  console.log("🔌 Client connected:", socket.id);
+  // console.log("🔌 Client connected:", socket.id);
   registerEvents(io, socket);
 });
-
-server.listen(4000, () => {
-  console.log("🚀 Backend with Express + Socket.IO running on http://localhost:4000");
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`🚀 Backend with Express + Socket.IO running on ${PORT}`);
 });
